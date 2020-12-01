842345693525542ed82593e5f5616ed9515f3bdb
Stephannie Jimenez <steff456@hotmail.com>
2020-12-01 02:20:45 -0500

    day 1

---
+++ aof2020/lib/aof2020.ex
@@ -0,0 +1,18 @@
+defmodule Aof2020 do
+  @moduledoc """
+  Documentation for `Aof2020`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Aof2020.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ aof2020/test/aof2020_test.exs
@@ -0,0 +1,8 @@
+defmodule Aof2020Test do
+  use ExUnit.Case
+  doctest Aof2020
+
+  test "greets the world" do
+    assert Aof2020.hello() == :world
+  end
+end
